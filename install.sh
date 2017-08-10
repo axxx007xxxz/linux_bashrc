92bbc9bdb6eea8dc31fecb1dc9b1d6e03714921a
axxx007xxxz <mattiabrsrn7@gmail.com>
2017-08-10 02:01:45 +0200

    Add an installation script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ $(whoami) != root ]; then
+	echo "ERROR: You must run this script as root!"
+	exit
+fi
+
+echo "WARNING: This script is supposed to be used on Fedora."
+echo "Pressing CTRL+C within three seconds will abort installation."
+sleep 3
+
+cp bashrc /etc/bashrc && chmod 644 /etc/bashrc
+cp bashrc.cust /etc/bashrc.cust && chmod 644 /etc/bashrc.cust
