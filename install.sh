876bee5c7e0157d4409cc5d3d7a5be01b0ecd5d5
axxx007xxxz <mattiabrsrn7@gmail.com>
2017-09-17 13:24:44 +0200

    install.sh: Print an empty line before installing

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ echo "WARNING: This script is supposed to be used on Fedora only."
 echo "Pressing CTRL+C within three seconds will abort installation."
 sleep 3
 
+echo ""
 cp bashrc /etc/bashrc && chmod 644 /etc/bashrc
 cp bashrc.cust /etc/bashrc.cust && chmod 644 /etc/bashrc.cust
 if [[ $1 = -f || $1 = --force  || ! -f /etc/bashrc.cust.priv ]]; then cp bashrc.cust.priv /etc/bashrc.cust.priv && chmod 644 /etc/bashrc.cust.priv; fi
