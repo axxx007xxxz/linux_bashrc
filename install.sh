ab538af9189b1a5c9aba3aa6f47ba1f054c072b4
axxx007xxxz <mattiabrsrn7@gmail.com>
2017-09-17 13:58:56 +0200

    Use quotation marks for variables' values

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ $UID != 0 ]; then
+if [ $UID != "0" ]; then
 	echo "ERROR: You must run this script as root!"
 	exit
 fi
@@ -11,5 +11,5 @@ sleep 3
 echo ""
 cp bashrc /etc/bashrc && chmod 644 /etc/bashrc
 cp bashrc.cust /etc/bashrc.cust && chmod 644 /etc/bashrc.cust
-if [[ $1 = -f || $1 = --force  || ! -f /etc/bashrc.cust.priv ]]; then cp bashrc.cust.priv /etc/bashrc.cust.priv && chmod 644 /etc/bashrc.cust.priv; else echo "WARNING: Skipping bashrc.cust.priv."; fi
+if [[ $1 = "-f" || $1 = "--force"  || ! -f /etc/bashrc.cust.priv ]]; then cp bashrc.cust.priv /etc/bashrc.cust.priv && chmod 644 /etc/bashrc.cust.priv; else echo "WARNING: Skipping bashrc.cust.priv."; fi
 echo "Done."
