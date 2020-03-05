c7fde5f821ec64f39a7449a876db4be4fd205ac9
axxx007xxxz <mattiabrsrn7@gmail.com>
2020-03-05 21:04:48 +0100

    Add an installation script
    Copied off master branch. Please refer to that if you're looking for commit history.

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [[ $UID != "0" ]]; then
+	echo "ERROR: You must run this script as root!"
+	exit 1
+fi
+
+echo "WARNING: This script is supposed to be used on Fedora only."
+echo "Pressing CTRL+C within three seconds will abort installation."
+sleep 3
+
+echo ""
+cp bashrc /etc/bashrc && chmod 644 /etc/bashrc
+cp bashrc.cust /etc/bashrc.cust && chmod 644 /etc/bashrc.cust
+echo "Done."
